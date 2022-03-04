3d6cda7bb6347af588a9c59607bda7f9e75d388a
nhutlmfx14950 <nhutlmfx14950@funix.edu.vn>
2022-03-05 00:37:25 +0700

    LAB02.1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h1>This is LAB02.1</h1>
         <p>Sample cho toàn bộ lab và project trong môn học Lập Trình Web Front-End với React với đầy đủ dependency.</p>
         <p>Phiên bản React hiện tại trên máy của bạn là: {React.version}</p>
         <p>Phiên bản React đã được sử dụng để thiết kế môn học: 16.14.0.</p>
